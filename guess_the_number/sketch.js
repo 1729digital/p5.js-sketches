3b2681da727c9c5be6eff93aeb6f0a67626b80e6
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-11-26 08:04:46 +0530

    guess the number- stupid game

---
+++ guess_the_number/sketch.js
@@ -0,0 +1,35 @@
+
+var slide;
+var sb;
+var x;
+var newGame;
+var para ;
+var result;
+
+function setup(){
+  x= floor(random(100));
+  newGame = createButton('....................New game...............');
+  newGame.mousePressed(newgamefunction);
+
+  para = createP('guess '+x);
+
+
+   slide = createSlider(0,100,50);
+   createP('');
+   sb = createButton(' .............................. Submit  .............................................');
+   sb.mousePressed(sbf);
+   createP('');
+   result = createP('');
+
+}
+function newgamefunction(){
+x= floor(random(100));
+para.html('guess '+x);
+result.html('');
+}
+function sbf(){
+  var off = abs((x - slide.value()));
+
+  result.html('you value was '+slide.value()+' you were off by '+off);
+
+}
