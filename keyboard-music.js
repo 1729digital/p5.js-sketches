bd3aacf8dfd6a780b4a86bea0c8837a21a3bff20
Prophet_MEOW <aradhyamakkar97@gmail.com>
2023-08-19 03:31:49 +0530

    Update keyboard-music.js

--- keyboard-music.js
+++ keyboard-music.js
@@ -2,6 +2,7 @@ let guitarStrings = [];
 let frequencies = [];
 let keys = "abcdefghijklmnopqrstuvwxyz0123456789";
 let keyPressedFlags = new Array(36).fill(false);
+let playedNotes = []; // Memory Store for playback feature
 
 // Recording variables
 let recorder;
@@ -9,33 +10,32 @@ let soundFile;
 let recording = false;
 let recordButton, saveButton;
 
+let playBackButton, stopPlaybackButton;
+let isPlayingBack = false;
+
 let row1 = "1234567890";
 let row2 = "qwertyuiop";
 let row3 = "asdfghjkl";
 let row4 = "zxcvbnm";
 let allRows = [row1, row2, row3, row4];
-let keyWidths = [50, 60, 70, 80];  // Slightly wider keys for each row
-let rowStarts = [100, 70, 120, 170];  // Adjusted starting x-positions for each row
+let keyWidths = [50, 60, 70, 80];
+let rowStarts = [100, 70, 120, 170];
 
 function setup() {
   createCanvas(800, 300);
   noLoop();
 
-  // Guitar string setup using oscillators for a plucked sound
-  let baseFrequency = 130.81;  // C3 in Hz
-  
+  // Guitar string setup
+  let baseFrequency = 130.81; // C3 in Hz
   for (let i = 0; i < 36; i++) {
     frequencies[i] = baseFrequency * Math.pow(2, i / 12);
-    
     let guitarString = {
-      osc: new p5.Oscillator('triangle'),  // Using a triangle wave oscillator
-      env: new p5.Envelope(0.001, 0.5, 0, 0.5)  // Quick attack and release for a plucked sound
+      osc: new p5.Oscillator('triangle'),
+      env: new p5.Envelope(0.001, 0.5, 0, 0.5)
     };
-    
     guitarString.osc.freq(frequencies[i]);
     guitarString.osc.amp(guitarString.env);
     guitarString.osc.start();
-    
     guitarStrings.push(guitarString);
   }
 
@@ -51,6 +51,15 @@ function setup() {
   saveButton = createButton('Save Recording');
   saveButton.position(130, 260);
   saveButton.mousePressed(saveSoundFile);
+
+  // Playback buttons
+  playBackButton = createButton('Play Back');
+  playBackButton.position(250, 260);
+  playBackButton.mousePressed(playBack);
+
+  stopPlaybackButton = createButton('Stop Playback');
+  stopPlaybackButton.position(340, 260);
+  stopPlaybackButton.mousePressed(stopPlayback);
 }
 
 function draw() {
@@ -65,16 +74,14 @@ function drawKeyboard() {
     for (let char of allRows[r]) {
       let index = keys.indexOf(char);
       if (keyPressedFlags[index]) {
-        fill(150, 150, 255);  // Color for a pressed key
+        fill(150, 150, 255);
       } else {
-        fill(255);  // Default key color
+        fill(255);
       }
-      // Drawing each key with rounded edges
-      rect(xPosition, yPosition, keyWidths[r] - 2, 40, 5);  // Last parameter is for rounded corners
-      
-      fill(0);  // Text color
-      textAlign(CENTER, CENTER);  // Centering the text both horizontally and vertically
-      text(char.toUpperCase(), xPosition + keyWidths[r] / 2, yPosition + 20);  // Convert char to uppercase for display
+      rect(xPosition, yPosition, keyWidths[r] - 2, 40, 5);
+      fill(0);
+      textAlign(CENTER, CENTER);
+      text(char.toUpperCase(), xPosition + keyWidths[r] / 2, yPosition + 20);
       xPosition += keyWidths[r];
     }
     yPosition += 50;
@@ -89,6 +96,7 @@ function keyPressed() {
 function playNote(index) {
   if (index >= 0 && index < 36) {
     guitarStrings[index].env.play();
+    playedNotes.push({ note: index, time: millis() });
     keyPressedFlags[index] = true;
     redraw();
     setTimeout(() => {
@@ -98,13 +106,37 @@ function playNote(index) {
   }
 }
 
+function playBack() {
+  if (isPlayingBack) return;
+  isPlayingBack = true;
+  let playbackStartTime = millis();
+  let i = 0;
+
+  let interval = setInterval(() => {
+    if (i >= playedNotes.length || !isPlayingBack) {
+      clearInterval(interval);
+      isPlayingBack = false;
+      return;
+    }
+
+    let elapsedTime = millis() - playbackStartTime;
+    if (elapsedTime >= playedNotes[i].time) {
+      playNote(playedNotes[i].note);
+      i++;
+    }
+  }, 10);
+}
+
+function stopPlayback() {
+  isPlayingBack = false;
+}
+
 function toggleRecording() {
   if (!recording) {
     recorder.record(soundFile);
     recordButton.html('Stop Recording');
   } else {
     recorder.stop();
-    // A slight delay to ensure the recording is fully processed before we allow saving or doing anything with it
     setTimeout(() => {
       recordButton.html('Start Recording');
     }, 100);
@@ -113,7 +145,7 @@ function toggleRecording() {
 }
 
 function saveSoundFile() {
-  if (soundFile && soundFile.duration() > 0) {  // Check if there's any recorded content
+  if (soundFile && soundFile.duration() > 0) {
     saveSound(soundFile, 'myRecording.wav');
   } else {
     alert("No recorded sound to save!");
